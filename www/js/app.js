5b2238e8ad40fc9247c1a0919449b28ac33a39d4
Jeado Ko <haibane84@gmail.com>
2014-11-12 12:40:19 +0900

    월간웹 12월호 셈플 ionic 앱 배포

---
+++ www/js/app.js
@@ -0,0 +1,50 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "templates/menu.html",
+      controller: 'AppCtrl'
+    })
+    .state('app.intro', {
+      url: "/intro",
+      views: {
+        'menuContent' : {
+          templateUrl: "templates/intro.html"
+        }
+      }
+    })
+    .state('app.list', {
+      url: "/list",
+      views: {
+        'menuContent' : {
+          templateUrl: "templates/tutorialList.html",
+          controller : "TutorialListCtrl"
+        }
+      }
+    });
+
+  $urlRouterProvider.otherwise('/app/intro');
+});
